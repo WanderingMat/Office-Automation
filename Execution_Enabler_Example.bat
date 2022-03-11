7660fe511ac9fe68be2febafe676842f1b6e7fdf
Matt McCallum <86280237+WanderingMat@users.noreply.github.com>
2022-03-11 11:53:42 +1100

    Add files via upload

---
+++ Execution_Enabler_Example.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+PowerShell.exe -ExecutionPolicy Bypass -Command "& '.\PS_Script_name.ps1'"
---
+++ addPrintersFromCSV.ps1
@@ -0,0 +1,7 @@
+﻿foreach ($printer in @(Import-Csv \\inserviofs.inservio.local\InservioData\Matt\PrinterList.csv)) {
+
+Add-PrinterPort -Name $printer.IPAddress -PrinterHostAddress $printer.IPAddress -ErrorAction SilentlyContinue
+
+Add-Printer -Name $printer.Printername -DriverName $printer.Driver -PortName $printer.IPAddress -Comment $printer.Comment -Location $printer.Location -ErrorAction SilentlyContinue
+
+}
