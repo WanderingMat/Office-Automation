fd4da3d2936f873c98d122b97df418f22c97147c
Matt McCallum <86280237+WanderingMat@users.noreply.github.com>
2022-03-11 14:04:50 +1100

    Add files via upload

---
+++ certificateInstall.ps1
@@ -0,0 +1,15 @@
+﻿if (Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object {$_.Thumbprint -eq "Enter Thumbprint here"})
+
+{
+
+Write-Host "Already Installed"
+
+}
+
+else
+
+{
+
+Import-Certificate -FilePath ".\Certificate Name" -CertStoreLocation Cert:\LocalMachine\Root -verbose
+
+}
